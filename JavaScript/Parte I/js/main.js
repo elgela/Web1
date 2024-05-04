1c8ac8b1cc13846d71c1bb70b501d8c80cbd2a5d
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-05-04 02:39:38 -0300

    parte I JS iniciado

---
+++ JavaScript/Parte I/js/main.js
@@ -0,0 +1,16 @@
+"use script";
+//1//
+function textoFijo() {
+    let nombre = "Marcelo";
+    let apellido = "Gelato";
+    alert("Nombre: " + nombre + "         " + "Apellido: " + apellido);
+}
+textoFijo();
+//2//
+let unDiv = document.getElementById("div1").innerHTML = "Párrafo 1";
+let dosDiv = document.getElementById("div2").innerHTML = "Párrafo 2";
+let tresDiv = document.getElementById("div3").innerHTML = "Párrafo 3";
+//3//
+let btn = document.getElementById("btn-nombreApellido");
+btn.addEventListener("click", textoFijo);
+//4//
